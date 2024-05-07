c0c7da275cb21e89c290820d2d1c4a6c5df2f429
SgNikhilJha <nikhiljha@spicegems.com>
2024-05-07 18:16:29 +0530

    prototype example

--- array /array.js
+++ array /array.js
@@ -20,12 +20,12 @@ let ul = `<ul>${options}</ul>`;
 //arr prototypes
 // Method	           Description
 // concat()	          Joins two or more arrays and returns a result.
-// toString()	        Converts an array to a string of (comma-separated) array values.
-// indexOf()	        Searches an element of an array and returns its position (index).
+// toString()	        Converts an array to a string of (comma-separated) array values. -> ['hello','my name','is','nikhil'].toString()
+// indexOf()	        Searches an element of an array and returns its position (index).-> ['phone','tele','radio'].indexOf('radio')
 // find()	            Returns the first value of the array element that passes a given test.
 // findIndex()	      Returns the first index of the array element that passes a given test.
 // forEach()	        Calls a function for each element.
 // includes()	        Checks if an array contains a specified element.
-// sort()	            Sorts the elements alphabetically in strings and ascending order in numbers.
-// slice()	          Selects part of an array and returns it as a new array.
-// splice()	          Removes or replaces existing elements and/or adds new elements.
+// sort()	            Sorts the elements alphabetically in strings and ascending order in numbers. -> [1,10,5,9,4].sort((a,b)=>a-b)
+// slice()	          Selects part of an array and returns it as a new array. -> ["a", "b", "c", "d", "e"].slice(1,3)
+// splice()	          Removes or replaces existing elements and/or adds new elements. -> ["a", "b", "c", "d", "e"].splice(1,3)
