9a3cf1cdbcdb0db109ed489077cd5676875dd200
SgNikhilJha <nikhiljha@spicegems.com>
2024-05-07 14:53:06 +0530

    array

---
+++ array /array.js
@@ -0,0 +1,9 @@
+//Slice Prototype
+
+let alpha = ["a", "b", "c", "d", "e"];
+// slice(i,n)
+alpha.slice(1, 3); // slice give's shallow copy between given range from (i) to exculding(n)
+
+//Splice
+let a = ["a", "b", "c", "d", "e"];
+a.splice(1, 3); // splice(i,n) manipulate original array splice delete value between given rangefrom i to n
